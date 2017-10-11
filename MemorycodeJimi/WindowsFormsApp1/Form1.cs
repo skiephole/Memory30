a2bb8fb548c15a79e66904ad8441c2002e126450
DESKTOP-BMVUJHI\jimiv <a@a.a>
2017-10-11 11:29:45 +0200

    eerste beginsels voor een werkend memory spel

---
+++ MemorycodeJimi/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<MemoryCard> paartjesLijst = new List<MemoryCard>();
+            for(int i = 1; i <= 8; i++)
+            {
+                paartjesLijst.Add(new MemoryCard(i));
+                paartjesLijst.Add(new MemoryCard(i));
+            }
+
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                
+            }
+        }
+    }
+}
---
+++ MemorycodeJimi/WindowsFormsApp1/MemoryCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class MemoryCard
+    {
+        bool flipped = false;
+        int pair;
+
+        public MemoryCard(int pair)
+        {
+            this.pair = pair;
+        }
+
+        public int Pair
+        {
+            get { return pair; }
+        }
+    }
+}
